590b29352a7ee1a850263d997564e612f5bdfd21
dmitriyBreyfogel <breyfogeld@bk.ru>
2025-06-08 20:17:47 +0300

    Реализован класс ошибок Error. Реализована структура Token

---
+++ calculationtools.cpp
@@ -0,0 +1,3 @@
+#include "calculationtools.h"
+
+CalculationTools::CalculationTools() {}
---
+++ calculationtools.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATIONTOOLS_H
+#define CALCULATIONTOOLS_H
+
+class CalculationTools
+{
+public:
+    CalculationTools();
+};
+
+#endif // CALCULATIONTOOLS_H
